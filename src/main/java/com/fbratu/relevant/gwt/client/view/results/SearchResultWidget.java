116cfd4d78e3de846bef9f51fe4e3d0021e165a2
Florin <florin@test.ro>
2013-11-09 04:04:30 +0100

    Search results panel rework: first verion, with one result

---
+++ src/main/java/com/fbratu/relevant/gwt/client/view/results/SearchResultWidget.java
@@ -0,0 +1,9 @@
+package com.fbratu.relevant.gwt.client.view.results;
+
+/**
+ * Widget displaying a single SearchResult
+ * Author: Florin
+ */
+public class SearchResultWidget {
+    // TODO
+}
--- src/main/java/com/fbratu/relevant/gwt/client/view/results/SearchResultsPanel.java
+++ src/main/java/com/fbratu/relevant/gwt/client/view/results/SearchResultsPanel.java
@@ -1,9 +1,16 @@
 package com.fbratu.relevant.gwt.client.view.results;
 
+import com.fbratu.relevant.gwt.client.Resources;
 import com.fbratu.relevant.gwt.client.listener.ISearchResultsListener;
 import com.fbratu.relevant.gwt.shared.dto.SearchResult;
+import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiHandler;
+import com.google.gwt.uibinder.client.UiTemplate;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.*;
 
 import java.util.List;
@@ -13,48 +20,43 @@ import java.util.List;
  */
 public class SearchResultsPanel extends Composite {
 
-    // lame: result is a String. shit.
-    private String searchResult;
+    @UiTemplate("SearchResultWidget.ui.xml")
+    interface SearchResultsUiBinder extends UiBinder<Widget, SearchResultsPanel>{}
 
-    // the results are displayed in a popup
-    private DialogBox popup;
+    private SearchResultsUiBinder uiBinder;
 
-    private Button closeButton;
+    @UiField
+    Anchor resultTitle;
+
+    @UiField
+    Label resultDescription;
 
     // the Listener
     private ISearchResultsListener searchResultsListener;
 
-    public SearchResultsPanel() {
-        // Create a popup dialog box
-        popup = new DialogBox();
-        popup.setText("Search results:");
-        popup.setAnimationEnabled(true);
-        closeButton = new Button("Close");
-        // We can set the id of a widget by accessing its Element
-        closeButton.getElement().setId("closeButton");
-        VerticalPanel dialogVPanel = new VerticalPanel();
-        dialogVPanel.addStyleName("dialogVPanel");
-        dialogVPanel.add(new HTML("<b>Here are the offers:</b>"));
-        dialogVPanel.add(new Label(searchResult));
-        dialogVPanel.setHorizontalAlignment(VerticalPanel.ALIGN_RIGHT);
-        dialogVPanel.add(closeButton);
-        popup.setWidget(dialogVPanel);
+    // URL container
+    private final StringBuilder urlPlaceholder;
 
-        // Add a handler to close the DialogBox
-        closeButton.addClickHandler(new ClickHandler() {
-            public void onClick(ClickEvent event) {
-                searchResultsListener.notifySearchResultsClosed();
-            }
-        });
-        initWidget(popup);
+    public SearchResultsPanel() {
+        uiBinder = GWT.create(SearchResultsUiBinder.class);
+        initWidget(uiBinder.createAndBindUi(this));
+        urlPlaceholder = new StringBuilder();
+    }
+
+    @UiHandler("resultTitle")
+    public void onClick(ClickEvent clickEvent) {
+        Window.Location.replace(urlPlaceholder.toString());
     }
 
     public void registerListener(ISearchResultsListener listener) {
         searchResultsListener = listener;
     }
 
-    // TODO acilea e tricky, vezi ca esti in lazy mode!
     public void setResults(List<SearchResult> results) {
-        this.searchResult = results.get(0).getDescription();
+        SearchResult result = results.get(0);
+        resultTitle.setText(result.getTitle());
+        resultDescription.setText(result.getDescription());
+        urlPlaceholder.setLength(0);
+        urlPlaceholder.append(result.getOriginalAnnounceLink());
     }
 }
