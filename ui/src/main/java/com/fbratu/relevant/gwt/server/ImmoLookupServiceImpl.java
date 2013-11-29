9b8e293939f50ac24c12ad3c6344589de80b2995
Florin <florin@test.ro>
2013-11-29 23:03:59 +0100

    dependency injection via xml file. no longer need app context gymnastics. beautiful\!

--- ui/src/main/java/com/fbratu/relevant/gwt/server/ImmoLookupServiceImpl.java
+++ ui/src/main/java/com/fbratu/relevant/gwt/server/ImmoLookupServiceImpl.java
@@ -4,13 +4,8 @@ import com.fbratu.relevant.gwt.shared.ImmoLookupService;
 import com.fbratu.relevant.ws.iface.ILookupService;
 import com.fbratu.relevant.ws.iface.LookupException;
 import com.fbratu.relevant.ws.iface.SearchCriteria;
-import com.fbratu.relevant.ws.iface.SearchResult;
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Service;
-import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.context.support.WebApplicationContextUtils;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,12 +18,14 @@ import java.util.List;
 public class ImmoLookupServiceImpl extends RemoteServiceServlet implements
         ImmoLookupService {
 
+    private ILookupService webServiceRef;
+
+    public ImmoLookupServiceImpl(ILookupService webServiceRef) {
+        this.webServiceRef = webServiceRef;
+    }
+
     public List<com.fbratu.relevant.gwt.shared.SearchResult>
     searchOffers(String location) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("file:///C:/dev/projects/RelevantI/" +
-                "ui/src/main/webapp/WEB-INF/applicationContext.xml");
-        // reference to the WS client
-        ILookupService webServiceRef = (ILookupService)context.getBean("seLogerLookupService");
         try {
             SearchCriteria searchCriteria = new SearchCriteria();
             searchCriteria.setLocation(location);
