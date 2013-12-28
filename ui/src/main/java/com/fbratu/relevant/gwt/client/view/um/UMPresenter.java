6b9dabd80a16705cbe695cbc4e74ecd269debf51
Florin <florin@test.ro>
2013-12-28 21:14:01 +0100

    UMPresenter: error handling

--- ui/src/main/java/com/fbratu/relevant/gwt/client/view/um/UMPresenter.java
+++ ui/src/main/java/com/fbratu/relevant/gwt/client/view/um/UMPresenter.java
@@ -1,5 +1,6 @@
 package com.fbratu.relevant.gwt.client.view.um;
 
+import com.fbratu.relevant.gwt.client.view.ErrorView;
 import com.fbratu.relevant.gwt.client.view.um.login.LoginController;
 import com.fbratu.relevant.gwt.shared.UserManagementService;
 import com.fbratu.relevant.gwt.shared.UserManagementServiceAsync;
@@ -17,25 +18,34 @@ public class UMPresenter implements LoginController{
 
     private final UserManagementServiceAsync userManagementService;
 
+    // another hack - this presenter uses its own ErrorView instance
+    // TODO centralize error and loading view
+    private final ErrorView errorView;
+
     public UMPresenter(UMPanel view) {
         // init server side proxy
         userManagementService = GWT.create(UserManagementService.class);
         this.view = view;
+        this.errorView = new ErrorView();
     }
 
     public void performLogin(String username, String password) {
         userManagementService.login(username, password, new AsyncCallback<Boolean>() {
             @Override
             public void onFailure(Throwable caught) {
-                Window.alert("Login failed");
+                errorView.show(caught);
             }
 
             @Override
             public void onSuccess(Boolean result) {
-                if(result)
+                if(result) {
+                    // TODO show signed page
                     Window.alert("Login succesful!");
-                else
-                    Window.alert("Login failed");
+                }
+                else {
+                    // TODO show in login panel
+                    errorView.show("Login failed");
+                }
             }
         });
     }
